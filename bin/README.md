07370dac2ea1f02dd1394154269ea9544b55b6c3
Andi Dwi <61771890+andiads@users.noreply.github.com>
2020-05-09 12:56:50 +0700

    inital project setup
    init spring boot project
    ==============
    dependencies used:
    > spring boot web
    > thymeleaf
    > spring jpa
    > mysql
    > devtools

---
+++ bin/README.md
@@ -0,0 +1,2 @@
+# SCMWebApp-Covid19-Indonesia
+Supply Chain Management Web Application Covid19 Indonesia
---
+++ src/main/java/com/nusacamp/app/ScmCovid19IdApplication.java
@@ -0,0 +1,13 @@
+package com.nusacamp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScmCovid19IdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScmCovid19IdApplication.class, args);
+	}
+
+}
